f7501a31c98900a1d01c3d13bbc4c5a68a24064e
mleblanc94 <leblancmichael94@gmail.com>
2021-08-03 22:26:24 -0400

    added Javascript

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let input = document.querySelector('.input-value');
 let errorMessage = document.querySelector('.error-message');
 let cityName = document.querySelector('.city-name');
 let temperature = document.querySelector('.temperature');
+let sky = document.querySelector('.sky');
+let skyPicture = document.querySelector('.skyPicture');
 
 
 submit.addEventListener('click', getWeather);
@@ -15,18 +17,31 @@ async function getWeather() {
        apiWeather = await response.json();
        console.log(apiWeather);
        populateTemperature();
+       atmosphere();
       //  warningMessage();
     } catch(error) {
         console.log("an error has occurred");
           }
        }
 
-
+// Populating the Temperature on the main page
 let populateTemperature = () => {
    let initialRawTemperature = apiWeather.main.temp;
    let rawTemperature = (initialRawTemperature - 273.15) * 1.8 + 32;
    let realTemperature = (Math.round(rawTemperature));
    temperature.innerHTML = realTemperature+"<span>&#176;</span>";
+}
+
+// Populating the Atmoshphere on the main page (Sunny, Cloudy, Overcast, etc.)
+let atmosphere = () => {
+   let initialAtmosphere = apiWeather.weather[0].description;
+   sky.innerHTML = initialAtmosphere;
+   if (initialAtmosphere === "overcast clouds") {
+      skyPicture.innerHTML = '<i class="fas fa-cloud"></i>';
+   } else if (initialAtmosphere === "light rain") {
+      skyPicture.innerHTML = '<i class="fas fa-cloud-rain"></i>';
+   }
+   console.log(initialAtmosphere);
 }
 
 
