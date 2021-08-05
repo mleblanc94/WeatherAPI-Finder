6c0e113c4adf1e90f8f461cb382527b9f026ad8e
mleblanc94 <leblancmichael94@gmail.com>
2021-08-04 22:16:18 -0400

    added another weather possibility

--- script.js
+++ script.js
@@ -60,6 +60,10 @@ let atmosphere = () => {
       skyPicture.innerHTML = '<i class="fas fa-cloud-rain"></i>';
       body.style.background = 'url(rainClouds.webp) no-repeat center center fixed';
       body.style.backgroundSize = 'cover';
+   } else if (initialAtmosphere === "scattered clouds") {
+      skyPicture.innerHTML = '<i class="fas fa-cloud""></i>';
+      body.style.background = 'url(cloudy.webp) no-repeat center center fixed';
+      body.style.backgroundSize = 'cover';
    }
    console.log(initialAtmosphere);
 }
